271f6024951028c74af8a6da64526cff06f70a9c
Mandi Wise <hello@mandiwise.com>
2020-07-26 10:01:16 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Apollo Federation with Custom Directives Demo
+
+**Installation:**
+
+```bash
+npm i && npm run dev
+```
+
+**Tutorial:**
+
+[Read the full tutorial on dev.to here]() 👩‍💻
---
+++ people/index.js
@@ -0,0 +1,42 @@
+import { ApolloServer, gql } from "apollo-server";
+import { buildFederatedSchema } from "@apollo/federation";
+
+import { people } from "../data.js";
+
+const port = 4001;
+
+const typeDefs = gql`
+  type Person @key(fields: "id") {
+    id: ID!
+    name: String
+  }
+
+  extend type Query {
+    person(id: ID!): Person
+    people: [Person]
+  }
+`;
+
+const resolvers = {
+  Person: {
+    __resolveReference(object) {
+      return people.find(person => person.id === object.id);
+    }
+  },
+  Query: {
+    person(_, { id }) {
+      return people.find(person => person.id === id);
+    },
+    people() {
+      return people;
+    }
+  }
+};
+
+const server = new ApolloServer({
+  schema: buildFederatedSchema([{ typeDefs, resolvers }])
+});
+
+server.listen({ port }).then(({ url }) => {
+  console.log(`People service ready at ${url}`);
+});
