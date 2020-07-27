92dbf6b2796ebd1d4ef074b241b1aa8f27c62aee
Mandi Wise <hello@mandiwise.com>
2020-07-27 10:12:10 -0700

    Update mocked data.

--- data.js
+++ data.js
@@ -1,7 +1,19 @@
 export const people = [
-  { id: "1", name: "Steven Spielberg" },
-  { id: "2", name: "Richard Dreyfuss" },
-  { id: "3", name: "Harrison Ford" }
+  {
+    id: "1",
+    name: "Steven Spielberg",
+    dateOfBirth: "1946-12-18T00:00:00+00:00"
+  },
+  {
+    id: "2",
+    name: "Richard Dreyfuss",
+    dateOfBirth: "1947-10-29T00:00:00+00:00"
+  },
+  {
+    id: "3",
+    name: "Harrison Ford",
+    dateOfBirth: "1942-07-13T00:00:00+00:00"
+  }
 ];
 
 export const films = [
@@ -9,18 +21,21 @@ export const films = [
     id: "1",
     title: "Jaws",
     actors: ["2"],
-    director: "1"
+    director: "1",
+    releaseDate: "1975-06-20T00:00:00+00:00"
   },
   {
     id: "2",
     title: "Close Encounters of the Third Kind",
     actors: ["2"],
-    director: "1"
+    director: "1",
+    releaseDate: "1977-11-15T00:00:00+00:00"
   },
   {
     id: "3",
     title: "Raiders of the Lost Ark",
     actors: ["3"],
-    director: "1"
+    director: "1",
+    releaseDate: "1981-06-21T00:00:00+00:00"
   }
 ];
